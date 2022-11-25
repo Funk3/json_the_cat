4b79d3d312d8c8770bb2b86a45d7d576ee5ed7bc
Zak <zak.johansson@gmail.com>
2022-11-24 16:25:24 -0800

    created working request and description print of breedfetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,2 +1,13 @@
 const request = require("request")
+const userInput = process.argv[2].slice(0,4)
+const url = `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`
+
+const breedFetcher = () => {
+  request(url, (error, response, body) => {
+    const data = JSON.parse(body);
+    console.log(data[0].description);
+  });
+};
+
+breedFetcher()
 
